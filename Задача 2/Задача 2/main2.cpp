e5cbecdbafedd6828c9763566f3e21781b676ffa
vivasuk683 <vivas@DESKTOP-BE431TS>
2025-01-18 11:58:27 +0300

    заяц

--- Задача 2/Задача 2/main2.cpp
+++ Задача 2/Задача 2/main2.cpp
@@ -3,14 +3,23 @@ using namespace std;
 
 int rabbit_option(const int& np, int kp = 3) {
 	int count = 0;
-	if (kp == 1) {
-		return np;
+	if (np == 0) {
+		return 1;
 	}
-	return rabbit_option(np,kp - 1)
+	else if (np < 1) {
+		return 0;
+	}
+	int options = 0;
+	for (int jump = 1; jump <= kp; ++jump) {
+		options += rabbit_option(np - jump, kp);
+	}
+
+	return options;
 }
 
 int main() {
 	int n, k;
 	cout << "Input the number of steps and the maximum jump of the rabbit ";
 	cin >> n >> k;
+	cout << rabbit_option(n, k);
 }
